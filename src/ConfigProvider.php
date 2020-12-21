33e09698a44a3dcf02fa8e535230b28633c8df40
alexzy <1003057931@qq.com>
2020-12-21 17:04:58 +0800

    增加数据库迁移工具文件

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
  */
 namespace Ycbl\AdminAuth;
 
+use Hyperf\Utils\Collection;
+use Hyperf\Utils\Filesystem\Filesystem;
+
 class ConfigProvider
 {
     public function __invoke(): array
@@ -29,13 +32,31 @@ class ConfigProvider
             ],
             'publish' => [
                 [
-                    'id' => 'admin_auth',
+                    'id' => 'config',
                     'description' => 'admin_auth 组件配置.', // 描述
                     // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                    'source' => __DIR__ . '/../publish/admin_auth.php',  // 对应的配置文件路径
+                    'source' => __DIR__ . '/../publish/config/admin_auth.php',  // 对应的配置文件路径
                     'destination' => BASE_PATH . '/config/autoload/admin_auth.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'database',
+                    'description' => 'admin_auth 数据库迁移工具.', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/../publish/database/admin_auth.php.stub',  // 对应的配置文件路径
+                    'destination' => $this->getMigrationFileName(), // 复制为这个路径下的该文件
                 ],
             ],
         ];
+    }
+
+    protected function getMigrationFileName(): string
+    {
+        $timestamp = date('Y_m_d_His');
+        $filesystem = new Filesystem();
+        return Collection::make(BASE_PATH . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path . '*_create_auth_tables.php');
+            })->push(BASE_PATH . "/migrations/{$timestamp}_create_permission_tables.php")
+            ->first();
     }
 }
